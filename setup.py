f924e7143260c6593798f16c047078919095243c
Arne Schauf <arne@as-webservices.de>
2013-12-17 22:35:02 +0100

    2 basic templates, template project and attempt for pip package

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from distutils.core import setup
+
+setup(
+    name='django-cms-bootstrap-templates',
+    version='0.0.1a',
+    author=u'Arne Schauf',
+    author_email='python.asmaps.de',
+    packages=['template_project.cms_bootstrap_templates'],
+    url='https://github.com/asmaps/django-cms-bootstrap-templates',
+    license='MIT licence, see LICENCE file',
+    description='A set of bootstrap3 templates for use with django-cms',
+    long_description=open('README.md').read(),
+    zip_safe=False,
+    include_package_data=True
+)
